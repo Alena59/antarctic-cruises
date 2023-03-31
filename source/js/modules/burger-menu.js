df00f313fcd6c58ee6cb2627466050bcbc4f3a08
Alena59 <asdalena59@gmail.com>
2023-03-31 21:31:55 +0300

    header

---
+++ source/js/modules/burger-menu.js
@@ -0,0 +1,52 @@
+let header = document.querySelector('.header');
+let navToggle = document.querySelector('.nav__toggle');
+const body = document.getElementById('body');
+const logo = document.querySelector('.header__link-logo');
+
+header.classList.remove('is-nojs');
+let lastFocus;
+lastFocus = document.activeElement;
+
+function focus(e) {
+  if (e.keyCode === 9) {
+    let focusable = header.querySelectorAll('a, button');
+    if (focusable.length) {
+      let first = focusable[0];
+      let last = focusable[focusable.length - 1];
+      let shift = e.shiftKey;
+      if (shift) {
+        if (e.target === first) {
+          last.focus();
+          e.preventDefault();
+        }
+      } else {
+        if (e.target === last) {
+          first.focus();
+          e.preventDefault();
+        }
+      }
+    }
+  }
+}
+
+function initMenu() {
+  navToggle.addEventListener('click', function () {
+    if (header.classList.contains('is-close')) {
+      header.classList.remove('is-close');
+      header.classList.add('is-open');
+      body.style.overflow = 'hidden';
+      logo.focus();
+
+      header.addEventListener('keydown', focus);
+
+    } else {
+      header.classList.add('is-close');
+      header.classList.remove('is-open');
+      body.style.overflow = 'scroll';
+      header.removeEventListener('keydown', focus);
+      lastFocus.focus();
+    }
+    })
+  }
+
+export {initMenu};
